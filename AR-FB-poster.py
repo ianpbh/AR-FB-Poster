8343f254e69ab8f1237af479f507b9302856bd73
Ian <ianpbh.hernandez@gmail.com>
2019-06-19 03:09:10 -0400

    Adicionada funcionalidade para entrar em grupo, digitar titulo, preço, e descrição

--- AR-FB-poster.py
+++ AR-FB-poster.py
@@ -1,12 +1,6 @@
 from selenium import webdriver
 import Constantes as dados
 
-mensagemPost = input('Digite a mensagem do post: ')
-opcao = input("Digite a opção: \n1) Postar no feed \n2) Postar em grupos\n")
-
-print("Iniciando serviço web...")
-navegador = webdriver.Firefox()
-
 def realizaLogin(email, senha):
     navegador.get('http://facebook.com.br')
     elementEmail = navegador.find_element_by_id('email')
@@ -24,16 +18,32 @@ def realizaPostagem(mensagem):
     navegador.find_element_by_tag_name('body').send_keys(mensagem);
     navegador.find_element_by_class_name('_6c0o').click()
 
-def realizaPostagemGrupos(mensagem):
+def realizaPostagemGrupos(titulo, descricao, preco):
     navegador.get('http://facebook.com.br/groups/2124656980892699')
     for x in range(3):
         navegador.find_element_by_xpath('//input[@placeholder="O que você está vendendo?"]').click()
     
-    navegador.find_element_by_xpath('//input[@placeholder="O que você está vendendo?"]').send_keys("teste")
-    navegador.find_element_by_xpath('//input[@placeholder="Preço"]').send_keys("123")
+    navegador.find_element_by_xpath('//input[@placeholder="O que você está vendendo?"]').send_keys(titulo)
+    navegador.find_element_by_xpath('//input[@placeholder="Preço"]').send_keys(preco)
     navegador.find_element_by_xpath('//input[@placeholder="Adicionar localização (opcional)"]').clear()
-    navegador.find_element_by_tag_name('body').send_keys('descricao');
-    # navegador.find_element_by_xpath('//input[@placeholder="Descreva seu item (opcional)"]').send_keys("descrição")
+    navegador.find_element_by_class_name('_1mwp').click()
+    navegador.find_element_by_tag_name('body').send_keys(descricao);
+
+
+opcao = input("Digite a opção: \n1) Postar no feed \n2) Postar em grupos\n")
+
+if opcao == "1":
+    mensagemPost = input('Digite a mensagem do post: ')
+elif opcao == "2":
+    tituloAnuncio = input('Digite o título do anuncio do post: ')
+    descricaoAnuncio = input('Digite a descrição do anuncio do post: ')
+    precoAnuncio = input('Digite o preço do anuncio do post: ')
+else:
+    print("Opção inválida")
+    exit()
+
+print("Iniciando serviço web...")
+navegador = webdriver.Firefox()
 
 email = dados.email()
 senha = dados.senha()
@@ -42,6 +52,7 @@ realizaLogin(email, senha)
 if opcao == "1":
     realizaPostagem(mensagemPost)
 elif opcao == "2":
-    realizaPostagemGrupos(mensagemPost)
+    realizaPostagemGrupos(tituloAnuncio, descricaoAnuncio, precoAnuncio)
 else:
     print("Opção inválida")
+    exit()

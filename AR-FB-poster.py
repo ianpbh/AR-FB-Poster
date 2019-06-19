b8081d86cc85795c8039e964ebc6d4095056e361
Ian <ianpbh.hernandez@gmail.com>
2019-06-19 02:44:36 -0400

    iniciada a função para postar em grupos

--- AR-FB-poster.py
+++ AR-FB-poster.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import Constantes as dados
 
 mensagemPost = input('Digite a mensagem do post: ')
+opcao = input("Digite a opção: \n1) Postar no feed \n2) Postar em grupos\n")
 
 print("Iniciando serviço web...")
 navegador = webdriver.Firefox()
@@ -23,8 +24,24 @@ def realizaPostagem(mensagem):
     navegador.find_element_by_tag_name('body').send_keys(mensagem);
     navegador.find_element_by_class_name('_6c0o').click()
 
+def realizaPostagemGrupos(mensagem):
+    navegador.get('http://facebook.com.br/groups/2124656980892699')
+    for x in range(3):
+        navegador.find_element_by_xpath('//input[@placeholder="O que você está vendendo?"]').click()
+    
+    navegador.find_element_by_xpath('//input[@placeholder="O que você está vendendo?"]').send_keys("teste")
+    navegador.find_element_by_xpath('//input[@placeholder="Preço"]').send_keys("123")
+    navegador.find_element_by_xpath('//input[@placeholder="Adicionar localização (opcional)"]').clear()
+    navegador.find_element_by_tag_name('body').send_keys('descricao');
+    # navegador.find_element_by_xpath('//input[@placeholder="Descreva seu item (opcional)"]').send_keys("descrição")
+
 email = dados.email()
 senha = dados.senha()
 realizaLogin(email, senha)
-realizaPostagem(mensagemPost)
-navegador.quit()
+
+if opcao == "1":
+    realizaPostagem(mensagemPost)
+elif opcao == "2":
+    realizaPostagemGrupos(mensagemPost)
+else:
+    print("Opção inválida")

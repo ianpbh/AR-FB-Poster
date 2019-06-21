5acd8be40ecb862c97cae346ad62545a88b99756
Ian <ianpbh.hernandez@gmail.com>
2019-06-21 04:09:07 -0400

    Adicionada a opção de abrir o navegador ou rodar em headless. Tratada a entrada de dados inválida no início do script

--- AR-FB-poster.py
+++ AR-FB-poster.py
@@ -4,22 +4,41 @@ import Funcoes
 import os
 import time
 
-opcao = input("Digite a opção: \n1) Postar no feed \n2) Postar em grupos\n")
-if opcao == "1":
-    mensagemPost = input('Digite a mensagem do post: ')
-elif opcao == "2":
-    tituloAnuncio = input('Digite o título do anuncio do post: ')
-    descricaoAnuncio = input('Digite a descrição do anuncio do post: ')
-    precoAnuncio = input('Digite o preço do anuncio do post: ')
-    imagemAnuncio = input('Digite o nome da imagem que deseja fazer upload, vazio caso não deseje postar imagem (Precisa estar dentro da pasta images): ')
-else:
-    print("Opção inválida")
-    exit()
+testerNavegador = True
+while testerNavegador == True:
+    opcaoNavegador = input("Deseja visualizar o navegador? (S/N) ")
+    if opcaoNavegador.upper() == "S" or opcaoNavegador.upper() == "N":
+        testerNavegador = False
+    else:
+        print("Opção inválida")
+
+
+testerOpcao = True
+while testerOpcao == True:
+    opcao = input("Digite a opção: \n1) Postar no feed \n2) Postar em grupos\n")
+    if opcao == "1":
+        mensagemPost = input('Digite a mensagem do post: ')
+        testerOpcao = False
+    elif opcao == "2":
+        tituloAnuncio = input('Digite o título do anuncio do post: ')
+        descricaoAnuncio = input('Digite a descrição do anuncio do post: ')
+        precoAnuncio = input('Digite o preço do anuncio do post: ')
+        imagemAnuncio = input('Digite o nome da imagem que deseja fazer upload, vazio caso não deseje postar imagem (Precisa estar dentro da pasta images): ')
+        testerOpcao = False
+    else:
+        print("Opção inválida")
+
 
 print("Iniciando serviço web...")
-navegador = webdriver.Firefox()
-funcoes = Funcoes.FuncoesNavegador()
 
+if opcaoNavegador.upper() == "S":
+    navegador = webdriver.Firefox()
+else:
+    opcoes = webdriver.FirefoxOptions()
+    opcoes.add_argument('-headless')
+    navegador = webdriver.Firefox(options=opcoes)
+
+funcoes = Funcoes.FuncoesNavegador()
 email = dados.email()
 senha = dados.senha()
 funcoes.realizaLogin(navegador, email, senha)

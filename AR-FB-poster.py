c9510d4538fbaf24af507ad365a975a2d353f522
ianpbh <ianpbh.hernandez@gmail.com>
2019-07-17 22:55:48 -0300

    Alterada a forma como as cores sao tratadas

--- AR-FB-poster.py
+++ AR-FB-poster.py
@@ -5,16 +5,7 @@ import os
 import time
 import json
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
+bcolors = Funcoes.bcolors
 jsonPost = open("post.json", "r")
 descricaoPost = json.loads(jsonPost.read())
 
